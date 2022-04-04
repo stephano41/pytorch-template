011c0ffafccd9c0feaef045fe364042a70141f45
Steven <stevenzhangshao@gmail.com>
2022-04-04 18:45:44 +0930

    weighted data loader

--- base/base_data_loader.py
+++ base/base_data_loader.py
@@ -1,13 +1,15 @@
 import numpy as np
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, WeightedRandomSampler, Subset
 from torch.utils.data.dataloader import default_collate
 from torch.utils.data.sampler import SubsetRandomSampler
+import torch
 
 
 class BaseDataLoader(DataLoader):
     """
     Base class for all data loaders
     """
+
     def __init__(self, dataset, batch_size, shuffle, validation_split, num_workers, collate_fn=default_collate):
         self.validation_split = validation_split
         self.shuffle = shuffle
@@ -59,3 +61,70 @@ class BaseDataLoader(DataLoader):
             return None
         else:
             return DataLoader(sampler=self.valid_sampler, **self.init_kwargs)
+
+
+def _weighted_sampler(targets, subset_idx):
+    class_sample_count = torch.tensor(
+        [(targets[subset_idx] == t).sum() for t in torch.unique(targets, sorted=True)])
+    weight = 1. / class_sample_count.float()
+    samples_weight = torch.tensor([weight[t] for t in targets[subset_idx]])
+
+    return WeightedRandomSampler(samples_weight, len(samples_weight))
+
+
+class WeightedDataLoader(DataLoader):
+    """
+    Base class for all data loaders
+    """
+
+    def __init__(self, dataset, batch_size, shuffle, validation_split, num_workers, targets,
+                 collate_fn=default_collate):
+        self.validation_split = validation_split
+        self.shuffle = shuffle
+
+        self.batch_idx = 0
+        self.n_samples = len(dataset)
+
+        self.sampler, train_idx, valid_idx = self._split_sampler(self.validation_split, targets)
+
+        train_set = Subset(dataset, train_idx)
+        self.valid_set = Subset(dataset, valid_idx)
+
+        self.init_kwargs = {
+            'batch_size': batch_size,
+            'shuffle': self.shuffle,
+            'collate_fn': collate_fn,
+            'num_workers': num_workers
+        }
+        super().__init__(sampler=self.sampler, pin_memory=True, dataset=train_set, **self.init_kwargs)
+
+    # make it not random
+    def _split_sampler(self, split, targets):
+        if split == 0.0:
+            return None, None
+
+        idx_full = np.arange(self.n_samples)
+
+        np.random.seed(0)
+        np.random.shuffle(idx_full)
+
+        if isinstance(split, int):
+            assert split > 0
+            assert split < self.n_samples, "validation set size is configured to be larger than entire dataset."
+            len_valid = split
+        else:
+            len_valid = int(self.n_samples * split)
+
+        valid_idx = idx_full[0:len_valid]
+        train_idx = np.delete(idx_full, np.arange(0, len_valid))
+
+        train_sampler = _weighted_sampler(targets, train_idx)
+
+        # turn off shuffle option which is mutually exclusive with sampler
+        self.shuffle = False
+        self.n_samples = len(train_idx)
+
+        return train_sampler, train_idx, valid_idx
+
+    def split_validation(self):
+        return DataLoader(pin_memory=True, dataset=self.valid_set, **self.init_kwargs)
